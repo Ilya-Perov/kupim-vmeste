49257d1bcf964b5e7185e02cc61630940544ecc8
Ilya Perov <petrovilya2013@gmail.com>
2026-02-19 10:26:53 +0300

    Изменены прегенерированные фалйы CRA, добавлена начальная страницы, добавлен начальный роутинг

---
+++ frontend/src/components/home/home.js
@@ -0,0 +1,13 @@
+import "./home.css";
+
+const Home = () =>{
+    return(
+        <div className="home">
+            <div className="homeTitle">
+                Домашняя страница
+            </div>
+        </div>
+    );
+};
+
+export default Home;
