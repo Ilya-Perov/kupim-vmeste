1e48ad8419e3ab4e65ba23fb9c349e3d97405afe
Ilya Perov <petrovilya2013@gmail.com>
2026-02-27 16:34:05 +0300

    Создана новая страница аккаунта. Для нее настроен роутинг

---
+++ frontend/src/components/account/account.js
@@ -0,0 +1,13 @@
+import "./account.css";
+
+const Account = () =>{
+    return(
+        <div className="account">
+            <div className="accountTitle">
+                Страница для аккаунта
+            </div>
+        </div>
+    );
+};
+
+export default Account;
